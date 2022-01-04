4335c0ccec23241ab95119aef9f00eca43c3ef5f
ShiftySlothe <forson.charlesj@gmail.com>
2022-01-04 13:09:33 +0000

    Updated group likes schema to group tvshows and movies together

--- src/server/routers/groupLikes.ts
+++ src/server/routers/groupLikes.ts
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 import { checkLoggedIn } from '../utils/queryHelpers';
 
 export const groupLikesRouter = createRouter()
-  .mutation('userLikesTVShow', {
+  .mutation('userLikesShow', {
     input: Yup.object({
       groupId: Yup.number().required(),
       showId: Yup.number().required(),
@@ -16,7 +16,7 @@ export const groupLikesRouter = createRouter()
         where: {
           userID: ctx.session?.user?.id,
           userGroupID: input.groupId,
-          tvShows: {
+          shows: {
             some: {
               id: input.showId,
             },
@@ -41,7 +41,7 @@ export const groupLikesRouter = createRouter()
               id: input.groupId,
             },
           },
-          tvShows: {
+          shows: {
             connect: {
               id: input.showId,
             },
@@ -54,7 +54,7 @@ export const groupLikesRouter = createRouter()
               users: true,
               userGroupLikes: {
                 where: {
-                  tvShows: {
+                  shows: {
                     some: {
                       id: input.showId,
                     },
@@ -74,7 +74,7 @@ export const groupLikesRouter = createRouter()
             id: input.groupId,
           },
           data: {
-            tvShows: {
+            shows: {
               connect: {
                 id: input.showId,
               },
@@ -87,7 +87,7 @@ export const groupLikesRouter = createRouter()
       }
     },
   })
-  .mutation('userDislikesTVShow', {
+  .mutation('userDislikesShow', {
     input: Yup.object({
       groupId: Yup.number().required(),
       showId: Yup.number().required(),
@@ -100,7 +100,7 @@ export const groupLikesRouter = createRouter()
         where: {
           userID: ctx.session?.user?.id,
           userGroupID: input.groupId,
-          tvShows: {
+          shows: {
             some: {
               id: input.showId,
             },
@@ -125,7 +125,7 @@ export const groupLikesRouter = createRouter()
               id: input.groupId,
             },
           },
-          tvShows: {
+          shows: {
             connect: {
               id: input.showId,
             },
@@ -138,157 +138,7 @@ export const groupLikesRouter = createRouter()
               users: true,
               userGroupLikes: {
                 where: {
-                  tvShows: {
-                    some: {
-                      id: input.showId,
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      });
-
-      return dislike;
-    },
-  })
-  .mutation('userLikesMovie', {
-    input: Yup.object({
-      groupId: Yup.number().required(),
-      showId: Yup.number().required(),
-    }).required(),
-
-    async resolve({ ctx, input }) {
-      checkLoggedIn(ctx);
-
-      const likedAlready = await ctx.prisma.userGroupLikes.findFirst({
-        where: {
-          userID: ctx.session?.user?.id,
-          userGroupID: input.groupId,
-          movies: {
-            some: {
-              id: input.showId,
-            },
-          },
-          liked: true,
-        },
-      });
-
-      if (likedAlready) {
-        return likedAlready;
-      }
-
-      const like = await ctx.prisma.userGroupLikes.create({
-        data: {
-          user: {
-            connect: {
-              id: ctx?.session?.user.id,
-            },
-          },
-          userGroup: {
-            connect: {
-              id: input.groupId,
-            },
-          },
-          movies: {
-            connect: {
-              id: input.showId,
-            },
-          },
-          liked: true,
-        },
-        include: {
-          userGroup: {
-            include: {
-              users: true,
-              userGroupLikes: {
-                where: {
-                  tvShows: {
-                    some: {
-                      id: input.showId,
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      });
-
-      const allUsersLiked =
-        like.userGroup.users.length <= like.userGroup.userGroupLikes.length;
-      if (allUsersLiked) {
-        const newMatch = ctx.prisma.userGroup.update({
-          where: {
-            id: input.groupId,
-          },
-          data: {
-            movies: {
-              connect: {
-                id: input.showId,
-              },
-            },
-          },
-        });
-        return newMatch;
-      } else {
-        return like;
-      }
-    },
-  })
-  .mutation('userDislikesMovie', {
-    input: Yup.object({
-      groupId: Yup.number().required(),
-      showId: Yup.number().required(),
-    }).required(),
-
-    async resolve({ ctx, input }) {
-      checkLoggedIn(ctx);
-
-      const dislikedAlready = await ctx.prisma.userGroupLikes.findFirst({
-        where: {
-          userID: ctx.session?.user?.id,
-          userGroupID: input.groupId,
-          movies: {
-            some: {
-              id: input.showId,
-            },
-          },
-          liked: false,
-        },
-      });
-
-      if (dislikedAlready) {
-        return dislikedAlready;
-      }
-
-      const dislike = await ctx.prisma.userGroupLikes.create({
-        data: {
-          user: {
-            connect: {
-              id: ctx?.session?.user.id,
-            },
-          },
-          userGroup: {
-            connect: {
-              id: input.groupId,
-            },
-          },
-          movies: {
-            connect: {
-              id: input.showId,
-            },
-          },
-          liked: false,
-        },
-        include: {
-          userGroup: {
-            include: {
-              users: true,
-              userGroupLikes: {
-                where: {
-                  movies: {
+                  shows: {
                     some: {
                       id: input.showId,
                     },
