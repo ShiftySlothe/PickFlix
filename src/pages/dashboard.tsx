5407526f524c115935d6712f293d232b7b9ce75c
Charles <forson.charlesj@gmail.com>
2022-02-09 17:04:51 +0000

    Added not optimised

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from 'next';
 import Dashboard from '../page-components/Dashboard/Dashboard';
 import CheckLoggedIn from '../components/Helpers/Next-Auth/CheckLoggedIn';
+import { Flex, Text } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   return (
     <CheckLoggedIn>
+      <Flex
+        align={'center'}
+        justify={'center'}
+        w={'100vw'}
+        display={{ base: 'block', md: 'none' }}
+      >
+        <Text>This site is not currently optimised for mobile.</Text>
+      </Flex>
       <Dashboard />
     </CheckLoggedIn>
   );
